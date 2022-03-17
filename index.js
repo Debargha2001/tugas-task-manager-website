0ba9158a2a700a9d0c30abc9b0c0c1c7cd3059fa
Debargha Dutta <debarghadutta2001@gmail.com>
2022-03-17 21:12:06 +0530

    ready to deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 5000;
 
 // use express router
 const db = require('./config/mongoose');
